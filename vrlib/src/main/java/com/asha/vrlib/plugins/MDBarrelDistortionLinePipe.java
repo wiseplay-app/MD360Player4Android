a08f011abde136c0faaf47037bb9abafe5966d4b
Wiseplay <info@wiseplay.tv>
2017-06-04 12:21:12 +0200

    Revert "Apply a director filter in the barrel distortion plugin."
    This reverts commit 1bc929dab087495c84fd604fc347af08ca868709.

--- vrlib/src/main/java/com/asha/vrlib/plugins/MDBarrelDistortionLinePipe.java
+++ vrlib/src/main/java/com/asha/vrlib/plugins/MDBarrelDistortionLinePipe.java
@@ -7,7 +7,6 @@ import android.opengl.GLES20;
 import com.asha.vrlib.MD360Director;
 import com.asha.vrlib.MD360DirectorFactory;
 import com.asha.vrlib.MD360Program;
-import com.asha.vrlib.MDDirectorFilter;
 import com.asha.vrlib.MDVRLibrary;
 import com.asha.vrlib.common.VRUtil;
 import com.asha.vrlib.model.BarrelDistortionConfig;
@@ -59,7 +58,6 @@ public class MDBarrelDistortionLinePipe extends MDAbsLinePipe {
 
     @Override
     public void init(final Context context) {
-        mDirector.applyFilter(new MDDirectorFilter());
         mProgram.build(context);
         MDObject3DHelper.loadObj(context,object3D);
     }
