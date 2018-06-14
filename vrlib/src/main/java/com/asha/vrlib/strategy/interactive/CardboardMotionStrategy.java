ab25e9c11f5752edea438cdbb4d24528cad0e968
Wiseplay <info@wiseplay.tv>
2018-06-14 11:42:46 +0200

    Revert "Replaced CardboardMotionStrategy class with latest upstream version."
    This reverts commit b34607d626166ef9679310cd162dc3ab3f370aef.

--- vrlib/src/main/java/com/asha/vrlib/strategy/interactive/CardboardMotionStrategy.java
+++ vrlib/src/main/java/com/asha/vrlib/strategy/interactive/CardboardMotionStrategy.java
@@ -7,13 +7,14 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.opengl.Matrix;
 import android.util.Log;
-import android.view.Display;
-import android.view.WindowManager;
+import android.view.Surface;
 
 import com.asha.vrlib.MD360Director;
-import com.google.vrtoolkit.cardboard.sensors.DeviceSensorLooper;
-import com.google.vrtoolkit.cardboard.sensors.HeadTracker;
-import com.google.vrtoolkit.cardboard.sensors.SystemClock;
+import com.asha.vrlib.common.MDMainHandler;
+import com.google.vrtoolkit.cardboard.sensors.internal.OrientationEKF;
+import com.google.vrtoolkit.cardboard.sensors.internal.Vector3d;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by hzqiujiadi on 16/3/19.
@@ -23,19 +24,29 @@ public class CardboardMotionStrategy extends AbsInteractiveStrategy implements S
 
     private static final String TAG = "CardboardMotionStrategy";
 
+    private int mDeviceRotation;
+
+    private float[] mResultMatrix = new float[16];
+
+    private float[] mTmpMatrix = new float[16];
+
+    private float[] mRotateMatrix = new float[16];
+
+    private float[] mEkfToHeadTracker = new float[16];
+
     private boolean mRegistered = false;
 
     private Boolean mIsSupport = null;
 
-    private float[] mTmpMatrix = new float[16];
+    private final OrientationEKF mTracker = new OrientationEKF();
 
-    private final Object matrixLock = new Object();
+    private Vector3d mLatestAcc = new Vector3d();
 
-    private HeadTracker headTracker;
+    private long mLatestGyroEventClockTimeNs;
 
-    private DeviceSensorLooper mDeviceSensorLooper;
+    private final Vector3d mGyroBias = new Vector3d();
 
-    private boolean isOn;
+    private final Vector3d mLatestGyro = new Vector3d();
 
     public CardboardMotionStrategy(InteractiveModeManager.Params params) {
         super(params);
@@ -91,10 +102,11 @@ public class CardboardMotionStrategy extends AbsInteractiveStrategy implements S
         return mIsSupport;
     }
 
-    private void registerSensor(Context context){
+    protected void registerSensor(Context context){
         if (mRegistered) return;
 
-        SensorManager mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        SensorManager mSensorManager = (SensorManager) context
+                .getSystemService(Context.SENSOR_SERVICE);
         Sensor sensor1 = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         Sensor sensor2 = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
@@ -103,73 +115,97 @@ public class CardboardMotionStrategy extends AbsInteractiveStrategy implements S
             return;
         }
 
-        if (mDeviceSensorLooper == null){
-            mDeviceSensorLooper = new DeviceSensorLooper(mSensorManager, getParams().mMotionDelay);
-        }
-
-        if (headTracker == null){
-            Display display = ((WindowManager)context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-            headTracker = new HeadTracker(mDeviceSensorLooper, new SystemClock(), display);
-        }
-
-        // start the tracker
-        mDeviceSensorLooper.registerListener(this);
-        headTracker.startTracking();
+        mSensorManager.registerListener(this, sensor1, getParams().mMotionDelay, MDMainHandler.sharedHandler());
+        mSensorManager.registerListener(this, sensor2, getParams().mMotionDelay, MDMainHandler.sharedHandler());
 
         mRegistered = true;
     }
 
-    private void unregisterSensor(Context context){
+    protected void unregisterSensor(Context context){
         if (!mRegistered) return;
 
-        // stop the tracker
-        mDeviceSensorLooper.unregisterListener(this);
-        headTracker.stopTracking();
+        SensorManager mSensorManager = (SensorManager) context
+                .getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager.unregisterListener(this);
 
         mRegistered = false;
     }
 
     @Override
     public void onSensorChanged(final SensorEvent event) {
-        if (isOn && event.accuracy != 0){
+        if (event.accuracy != 0){
             if (getParams().mSensorListener != null){
                 getParams().mSensorListener.onSensorChanged(event);
             }
 
-            synchronized (matrixLock){
-                Matrix.setIdentityM(mTmpMatrix, 0);
-                headTracker.getLastHeadView(mTmpMatrix, 0);
+            int type = event.sensor.getType();
+
+            if (type == Sensor.TYPE_ACCELEROMETER){
+                synchronized (mTracker){
+                    this.mLatestAcc.set(event.values[0], event.values[1], event.values[2]);
+                    this.mTracker.processAcc(this.mLatestAcc, event.timestamp);
+                }
+
+            } else if(type == Sensor.TYPE_GYROSCOPE){
+                synchronized (mTracker){
+                    this.mLatestGyroEventClockTimeNs = System.nanoTime();
+                    this.mLatestGyro.set(event.values[0], event.values[1], event.values[2]);
+                    Vector3d.sub(this.mLatestGyro, this.mGyroBias, this.mLatestGyro);
+                    this.mTracker.processGyro(this.mLatestGyro, event.timestamp);
+                }
             }
 
             getParams().glHandler.post(updateSensorRunnable);
         }
     }
 
+    private Runnable updateSensorRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (!mRegistered) return;
+
+            // mTracker will be used in multi thread.
+            synchronized (mTracker){
+                final double secondsSinceLastGyroEvent = TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - mLatestGyroEventClockTimeNs);
+                final double secondsToPredictForward = secondsSinceLastGyroEvent + 1.0/60;
+                final double[] mat = mTracker.getPredictedGLMatrix(secondsToPredictForward);
+                for (int i = 0; i < mat.length; i++){
+                    mTmpMatrix[i] = (float) mat[i];
+                }
+            }
+
+            float rotation = 0;
+            switch (mDeviceRotation){
+                case Surface.ROTATION_0:
+                    rotation = 0;
+                    break;
+                case Surface.ROTATION_90:
+                    rotation = 90.0f;
+                    break;
+                case Surface.ROTATION_180:
+                    rotation = 180.0f;
+                    break;
+                case Surface.ROTATION_270:
+                    rotation = 270.0f;
+                    break;
+            }
+
+            Matrix.setRotateEulerM(mRotateMatrix, 0, 0.0f, 0.0f, -rotation);
+            Matrix.setRotateEulerM(mEkfToHeadTracker, 0, -90.0f, 0.0f, rotation);
+
+            Matrix.multiplyMM(mResultMatrix, 0, mRotateMatrix, 0, mTmpMatrix , 0);
+            Matrix.multiplyMM(mTmpMatrix, 0, mResultMatrix, 0, mEkfToHeadTracker, 0);
+
+            for (MD360Director director : getDirectorList()){
+                director.updateSensorMatrix(mTmpMatrix);
+            }
+        }
+    };
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         if (getParams().mSensorListener != null){
             getParams().mSensorListener.onAccuracyChanged(sensor,accuracy);
         }
-
-        synchronized (matrixLock){
-            Matrix.setIdentityM(mTmpMatrix, 0);
-            headTracker.getLastHeadView(mTmpMatrix, 0);
-        }
-
-        getParams().glHandler.post(updateSensorRunnable);
     }
-
-    private Runnable updateSensorRunnable = new Runnable() {
-
-        @Override
-        public void run() {
-            if (!mRegistered || !isOn) return;
-
-            synchronized (matrixLock){
-                for (MD360Director director : getDirectorList()){
-                    director.updateSensorMatrix(mTmpMatrix);
-                }
-            }
-        }
-    };
 }
